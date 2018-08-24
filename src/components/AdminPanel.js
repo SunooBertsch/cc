7faf64d37f2012d831ba78b7049fff7f74532787
SunooBertsch <sbertsch92@yahoo.com>
2018-08-24 13:55:51 -0700

    AdminPanel displaying proper values

--- src/components/AdminPanel.js
+++ src/components/AdminPanel.js
@@ -1,99 +1,79 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import * as actions from "../actions";
-import { Button, Form, FormGroup, Label, Input, FormText } from "reactstrap";
+import {
+  Button,
+  Form,
+  FormGroup,
+  Label,
+  Input,
+  FormText,
+  Col
+} from "reactstrap";
 
 class AdminPanel extends Component {
   render() {
     return (
-      <Form>
-        <FormGroup>
-          <Label for="exampleEmail">Email</Label>
-          <Input
-            type="email"
-            name="email"
-            id="exampleEmail"
-            placeholder="with a placeholder"
-          />
-        </FormGroup>
-        <FormGroup>
-          <Label for="examplePassword">Password</Label>
-          <Input
-            type="password"
-            name="password"
-            id="examplePassword"
-            placeholder="password placeholder"
-          />
-        </FormGroup>
-        <FormGroup>
-          <Label for="exampleSelect">Select</Label>
-          <Input type="select" name="select" id="exampleSelect">
-            <option>1</option>
-            <option>2</option>
-            <option>3</option>
-            <option>4</option>
-            <option>5</option>
-          </Input>
-        </FormGroup>
-        <FormGroup>
-          <Label for="exampleSelectMulti">Select Multiple</Label>
-          <Input
-            type="select"
-            name="selectMulti"
-            id="exampleSelectMulti"
-            multiple>
-            <option>1</option>
-            <option>2</option>
-            <option>3</option>
-            <option>4</option>
-            <option>5</option>
-          </Input>
-        </FormGroup>
-        <FormGroup>
-          <Label for="exampleText">Text Area</Label>
-          <Input type="textarea" name="text" id="exampleText" />
-        </FormGroup>
-        <FormGroup>
-          <Label for="exampleFile">File</Label>
-          <Input type="file" name="file" id="exampleFile" />
-          <FormText color="muted">
-            This is some placeholder block-level help text for the above input.
-            It's a bit lighter and easily wraps to a new line.
-          </FormText>
-        </FormGroup>
-        <FormGroup tag="fieldset">
-          <legend>Radio Buttons</legend>
-          <FormGroup check>
-            <Label check>
-              <Input type="radio" name="radio1" /> Option one is this and
-              that—be sure to include why it's great
-            </Label>
-          </FormGroup>
-          <FormGroup check>
-            <Label check>
-              <Input type="radio" name="radio1" /> Option two can be something
-              else and selecting it will deselect option one
-            </Label>
-          </FormGroup>
-          <FormGroup check disabled>
-            <Label check>
-              <Input type="radio" name="radio1" disabled /> Option three is
-              disabled
-            </Label>
-          </FormGroup>
-        </FormGroup>
-        <FormGroup check>
-          <Label check>
-            <Input type="checkbox" /> Check me out
+      <Form style={{ margin: "20px" }}>
+        <FormGroup row>
+          <Label for="commission" sm={2}>
+            Commission:
           </Label>
+          <Col sm={2}>
+            <Input
+              type="number"
+              name="commission"
+              id="commission"
+              placeholder="0.00"
+            />
+          </Col>
+          <Label sm={2}> %</Label>
         </FormGroup>
-        <Button>Submit</Button>
+        <FormGroup row>
+          <Label for="surcharge" sm={2}>
+            Surcharge:
+          </Label>
+          <Col sm={2}>
+            <Input
+              type="number"
+              name="surcharge"
+              id="surcharge"
+              placeholder="0.00"
+            />
+          </Col>
+          <Label sm={2}> %</Label>
+        </FormGroup>
+        <FormGroup row>
+          <Label for="Minimal Commission" sm={2}>
+            Minimal Commission:
+          </Label>
+          <Col sm={2}>
+            <Input
+              type="number"
+              name="minimalCommission"
+              id="minimalCommission"
+              placeholder="0.00"
+            />
+          </Col>
+          <Label sm={2}> %</Label>
+        </FormGroup>
+        <FormGroup row>
+          <Label for="margin" sm={2}>
+            Buy/Sell rate margin:
+          </Label>
+          <Col sm={2}>
+            <Input type="number" name="margin" id="margin" placeholder="0.00" />
+          </Col>
+          <Label sm={2}> %</Label>
+        </FormGroup>
+        <Button onSubmit={this.props.updateRates}>Update</Button>
       </Form>
     );
   }
 }
-function mapStateToProps({reducer}) {
-  return{reducer}
+
+function mapStateToProps({ reducer }) {
+  return { reducer };
 }
 
 export default connect(mapStateToProps, actions)(AdminPanel);
